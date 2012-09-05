61465cc1c7f896ffa349069a439abc7fa95e08fa
Aaron McDaid <aaronmcdaid@gmail.com>
2012-09-05 12:13:36 +0100

    A proper test.  Two different function, different args, different results.

--- poly_lam_test.cpp
+++ poly_lam_test.cpp
@@ -3,13 +3,12 @@ using namespace std;
 #include "poly_lam.hpp"
 using namespace poly_lam;
 
-POLY_LAM_FIRST( add, x, y, [](T0 x,T1 y){ return x+y;} );
-template struct just_the_lambda_itself_add<int,int>;
+POLY_LAM_FIRST( add, x, y, return x+y; );
+POLY_LAM_FIRST( sub, w, z, return w-z; );
 
 int main() {
-	cout << add(3,4) << endl;
-	cout << add(3.0,4.0) << endl;
-	cout << add(3.3,4.4) << endl;
 	cout << add(5,"skip_the_start") << endl;
 	cout << add("skip_the_start",5) << endl;
+	cout << sub(5.5,4.4) << endl;
+	cout << sub("skip_the_start"+5,4) << endl;
 }
