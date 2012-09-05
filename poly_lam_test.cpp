e7d90fa437f96f272b7e85420d5d4aea2725a346
Aaron McDaid <aaronmcdaid@gmail.com>
2012-09-05 14:20:43 +0100

    Test perfect-forwarding with a test class (struct Noisy) that will print every time it is constructed.

--- poly_lam_test.cpp
+++ poly_lam_test.cpp
@@ -2,12 +2,34 @@
 using namespace std;
 #include "poly_lam.hpp"
 
+namespace x{
 POLY_LAM_FIRST( add, x, y, return x+y; );
 POLY_LAM_FIRST( sub, w, z, return w-z; );
+}
+
+struct Noisy {
+	int x_;
+	Noisy(int x) : x_(x) {
+		cout << "Noisy() " << x_ << endl;
+	}
+	Noisy(const Noisy &n) : x_(n.x_+1) {
+		cout << "Noisy(const Noisy &) " << x_ << endl;
+	}
+	Noisy(const Noisy &&n) : x_(n.x_+1) {
+		cout << "Noisy(const Noisy &&) " << x_ << endl;
+	}
+};
+Noisy operator+ (const Noisy& l, const Noisy& r) {
+	return Noisy(l.x_ + r.x_);
+}
 
 int main() {
-	cout << add(5,"skip_the_start") << endl;
-	cout << add("skip_the_start",5) << endl;
-	cout << sub(5.5,4.4) << endl;
-	cout << sub("skip_the_start"+5,4) << endl;
+	cout << x :: add(5,"skip_the_start") << endl;
+	cout << x :: add("skip_the_start",5) << endl;
+	cout << x :: sub(5.5,4.4) << endl;
+	cout << x :: sub("skip_the_start"+5,4) << endl;
+	cout << x :: add(Noisy(10), Noisy(200)).x_ << endl;
+	Noisy noisy10(10);
+	Noisy noisy200(200);
+	cout << x :: add(noisy10, noisy200).x_ << endl;
 }
