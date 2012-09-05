171fdc28cc141620f3387027efdff1f6c54a7065
Aaron McDaid <aaronmcdaid@gmail.com>
2012-09-05 11:40:28 +0100

    First working version.  I've still hardcoded the lambda though.

---
+++ poly_lam.hpp
@@ -0,0 +1,23 @@
+template <typename X> struct get_the_return_type;
+template <typename Ret, typename Class, typename ...Args> struct get_the_return_type< Ret (Class::*) (Args...)                > { typedef Ret return_type; };
+template <typename Ret, typename Class, typename ...Args> struct get_the_return_type< Ret (Class::*) (Args...) const          > { typedef Ret return_type; };
+template <typename Ret, typename Class, typename ...Args> struct get_the_return_type< Ret (Class::*) (Args...)       volatile > { typedef Ret return_type; };
+template <typename Ret, typename Class, typename ...Args> struct get_the_return_type< Ret (Class::*) (Args...) const volatile > { typedef Ret return_type; };
+
+template <typename T0, typename T1>
+struct poly_lam_struct {
+	typedef typeof( [](T0 x,T1 y){ return x+y;} ) lambda_type;
+	typedef decltype( &lambda_type :: operator())  fptr_t;
+	typedef get_the_return_type<fptr_t> x;
+	typedef typename get_the_return_type<fptr_t> :: return_type return_type;
+	static return_type execute(T0 t0, T1 t1) {
+		static lambda_type * const l_ptr = nullptr;
+		return (*l_ptr)(t0, t1);
+	}
+};
+
+template <typename T0, typename T1>
+auto pol(T0 t0, T1 t1) -> typename poly_lam_struct<T0,T1> :: return_type
+{
+	return poly_lam_struct<T0,T1> :: execute(t0, t1);
+}
---
+++ poly_lam_test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+#include "poly_lam.hpp"
+
+int main() {
+	cout << pol(3,4) << endl;
+	cout << pol(3.0,4.0) << endl;
+	cout << pol(3.3,4.4) << endl;
+}
