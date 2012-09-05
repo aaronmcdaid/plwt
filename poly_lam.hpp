0634da99c74d30d4175fa0cb830d440f297d28d3
Aaron McDaid <aaronmcdaid@gmail.com>
2012-09-05 11:48:03 +0100

    Split out 'just_the_lambda_itself'

--- poly_lam.hpp
+++ poly_lam.hpp
@@ -6,8 +6,13 @@ template <typename Ret, typename Class, typename ...Args> struct get_the_return_
 template <typename Ret, typename Class, typename ...Args> struct get_the_return_type< Ret (Class::*) (Args...) const volatile > { typedef Ret return_type; };
 
 template <typename T0, typename T1>
-struct poly_lam_struct {
+struct just_the_lambda_itself {
 	typedef typeof( [](T0 x,T1 y){ return x+y;} ) lambda_type;
+};
+
+template <typename T0, typename T1>
+struct all_the_other_details {
+	typedef typename just_the_lambda_itself<T0,T1> :: lambda_type lambda_type;
 	typedef decltype( &lambda_type :: operator())  fptr_t;
 	typedef get_the_return_type<fptr_t> x;
 	typedef typename get_the_return_type<fptr_t> :: return_type return_type;
@@ -17,9 +22,11 @@ struct poly_lam_struct {
 	}
 };
 
+// #define POLY_LAM_FIRST( t0, t1, lambda_text ) 
+
 template <typename T0, typename T1>
-auto pol(T0 t0, T1 t1) -> typename poly_lam_struct<T0,T1> :: return_type
+auto pol(T0 t0, T1 t1) -> typename all_the_other_details<T0,T1> :: return_type
 {
-	return poly_lam_struct<T0,T1> :: execute(t0, t1);
+	return all_the_other_details<T0,T1> :: execute(t0, t1);
 }
 }
--- poly_lam_test.cpp
+++ poly_lam_test.cpp
@@ -7,4 +7,6 @@ int main() {
 	cout << pol(3,4) << endl;
 	cout << pol(3.0,4.0) << endl;
 	cout << pol(3.3,4.4) << endl;
+	cout << pol(5,"skip_the_start") << endl;
+	cout << pol("skip_the_start",5) << endl;
 }
